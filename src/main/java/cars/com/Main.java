e32440e327705dab65bd6b51575f0e34607d6b1f
m.mrugas@itgenerator.pl <ITgen4ever>
2016-12-14 13:05:39 +0100

    main refactor

--- src/main/java/cars/com/Main.java
+++ src/main/java/cars/com/Main.java
@@ -2,19 +2,18 @@ package cars.com;
 
 import org.apache.log4j.BasicConfigurator;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-public class CarRental {
+public class Main {
 
     public static void main(String[] args) {
         try {
             KieServices ks = KieServices.Factory.get();
             BasicConfigurator.configure();
-            Logger.getLogger(CarRental.class).setLevel(Level.OFF);
+            Logger.getLogger(Main.class).setLevel(Level.OFF);
             KieContainer kContainer = ks.getKieClasspathContainer();
             KieSession session = kContainer.newKieSession("ksession-rules");
 
